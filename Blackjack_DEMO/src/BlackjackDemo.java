a4a4923ce5ff8a2b1087775e5dbfdc4552b5cbe0
Erosssore <111210204+Erosssore@users.noreply.github.com>
2022-11-24 22:21:31 -0600

    Delete Blackjack_DEMO directory
    Irrelevant directory

--- Blackjack_DEMO/src/BlackjackDemo.java
+++
@@ -1,9 +0,0 @@
-public class BlackjackDemo {
-    public static void main(String[] args) {
-        Deck card = new Deck();
-        card.shuffleDeck();
-
-        Cards goober = new Cards();
-        System.out.println(goober.cardValue(card.getCard(0)));
-    }
-}
